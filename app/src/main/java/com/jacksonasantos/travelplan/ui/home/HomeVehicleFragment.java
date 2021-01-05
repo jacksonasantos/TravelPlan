d1e8eae41c435304613a38fe4c3aad5ba1570028
Jackson Alessandro dos Santos <jackson_a_santos@hotmail.com>
2021-01-05 12:50:40 -0300

    ajustes HomeVehicle

--- app/src/main/java/com/jacksonasantos/travelplan/ui/home/HomeVehicleFragment.java
+++ app/src/main/java/com/jacksonasantos/travelplan/ui/home/HomeVehicleFragment.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,7 +30,6 @@ import com.jacksonasantos.travelplan.dao.VehicleGraphStatistics;
 import com.jacksonasantos.travelplan.ui.utility.Globals;
 import com.jacksonasantos.travelplan.ui.utility.Utils;
 import com.jacksonasantos.travelplan.ui.vehicle.FuelSupplyActivity;
-
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
@@ -53,10 +53,15 @@ public class HomeVehicleFragment extends Fragment implements View.OnClickListene
     private TextView tvFuelSupplyNumberLiters;
     private TextView tvFuelSupplyValue;
 
+    private ConstraintLayout layerStatisticsVehicle;
     private RecyclerView vehicleStatisticsList;
     private RecyclerView vehicleStatisticsTotalList;
     private GraphView graphStatistics;
+
+    private ConstraintLayout layerServiceVehicle;
     private RecyclerView maintenanceList;
+
+    private ConstraintLayout layerInsuranceVehicle;
     private RecyclerView insuranceList;
 
     DataPoint[] dataSeries;
@@ -77,15 +82,22 @@ public class HomeVehicleFragment extends Fragment implements View.OnClickListene
         imVehicleType = v.findViewById(R.id.imVehicleType);
         spVehicle =v.findViewById(R.id.spVehicle);
         tvLicencePlate = v.findViewById(R.id.tvLicencePlate);
+
         ImageButton btnRefuel = v.findViewById(R.id.btnRefuel);
         tvFuelSupplyDate = v.findViewById(R.id.tvFuelSupplyDate);
         tvFuelSupplyLastOdometer = v.findViewById(R.id.tvFuelSupplyLastOdometer);
         tvFuelSupplyNumberLiters = v.findViewById(R.id.tvFuelSupplyNumberLiters);
         tvFuelSupplyValue = v.findViewById(R.id.tvFuelSupplyValue);
+
+        layerStatisticsVehicle = v.findViewById(R.id.layerStatisticsVehicle);
         vehicleStatisticsList = v.findViewById(R.id.listVehicleStatistics);
         vehicleStatisticsTotalList = v.findViewById(R.id.listVehicleStatisticsTotal);
         graphStatistics = v.findViewById(R.id.graphStatistics);
+
+        layerServiceVehicle = v.findViewById(R.id.layerServiceVehicle);
         maintenanceList = v.findViewById(R.id.listInVehicleService);
+
+        layerInsuranceVehicle = v.findViewById(R.id.layerInsuranceVehicle);
         insuranceList = v.findViewById(R.id.listInsuranceExpiration);
 
         btnRefuel.setOnClickListener(new View.OnClickListener() {
@@ -120,92 +132,111 @@ public class HomeVehicleFragment extends Fragment implements View.OnClickListene
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                // Recupera dados do veículo selecionado no Spinner
+                // Recupera dados do veículo selecionado no Spinner - layerVehicle
                 vehicle[0] = (Vehicle) parent.getItemAtPosition(position);
                 tvLicencePlate.setText(vehicle[0].getLicense_plate());
                 imVehicleType.setImageResource(vehicle[0].getVehicleTypeImage(vehicle[0].getVehicle_type()));
 
                 g.setIdVehicle(vehicle[0].getId());
 
-                // Last Fuel Supply of Vehicle in Global selection
+                // Last Fuel Supply of Vehicle in Global selection - layerFuelSupply
                 FuelSupply fuelSupply = Database.mFuelSupplyDao.findLastFuelSupply( g.getIdVehicle() );
                 tvFuelSupplyDate.setText(Utils.dateToString(fuelSupply.getSupply_date()));
                 tvFuelSupplyLastOdometer.setText(getString(R.string.Odometer)+ ": "+fuelSupply.getVehicle_odometer());
                 tvFuelSupplyNumberLiters.setText(fuelSupply.getNumber_liters()==null? "0 "+g.getMeasureCapacity() :fuelSupply.getNumber_liters() +" "+g.getMeasureCapacity());
                 tvFuelSupplyValue.setText(currencyFormatter.format(fuelSupply.getSupply_value()==null?BigDecimal.ZERO:fuelSupply.getSupply_value()));
 
-                // Statistics of Vehicle in Global selection
+                // Statistics of Vehicle in Global selection - layerStatisticsVehicle
                 HomeVehicleStatisticsListAdapter adapterVehicle = new HomeVehicleStatisticsListAdapter(Database.mVehicleStatisticsDao.findLastVehicleStatistics(g.getIdVehicle()), getContext());
-                vehicleStatisticsList.setAdapter(adapterVehicle);
-                vehicleStatisticsList.setLayoutManager(new LinearLayoutManager(getContext()));
+                if (adapterVehicle.getItemCount() > 0 ) {
+                    layerStatisticsVehicle.setVisibility(View.VISIBLE);
+                    vehicleStatisticsList.setAdapter(adapterVehicle);
+                    vehicleStatisticsList.setLayoutManager(new LinearLayoutManager(getContext()));
 
-                HomeVehicleStatisticsListAdapter adapterTotalVehicle = new HomeVehicleStatisticsListAdapter(Database.mVehicleStatisticsDao.findTotalVehicleStatistics(g.getIdVehicle()), getContext());
-                vehicleStatisticsTotalList.setAdapter(adapterTotalVehicle);
-                vehicleStatisticsTotalList.setLayoutManager(new LinearLayoutManager(getContext()));
+                    HomeVehicleStatisticsListAdapter adapterTotalVehicle = new HomeVehicleStatisticsListAdapter(Database.mVehicleStatisticsDao.findTotalVehicleStatistics(g.getIdVehicle()), getContext());
+                    vehicleStatisticsTotalList.setAdapter(adapterTotalVehicle);
+                    vehicleStatisticsTotalList.setLayoutManager(new LinearLayoutManager(getContext()));
 
-                // Graph Statistics Vehicle - https://github.com/jjoe64/GraphView
-                graphStatistics.removeAllSeries();                   // Clear the Graph
-                graphStatistics.refreshDrawableState();
+                    // Graph Statistics Vehicle - https://github.com/jjoe64/GraphView
+                    graphStatistics.removeAllSeries();                   // Clear the Graph
+                    graphStatistics.onDataChanged(true, true);
 
-                // Series "Road"
-                dataSeries = getData(2);
-                if ( dataSeries.length > tamHorizontalLabels ) tamHorizontalLabels = dataSeries.length;
-                LineGraphSeries<DataPoint> series1 = new LineGraphSeries<>(dataSeries);
-                series1.setColor(Color.BLUE);
-                series1.setDrawDataPoints(true);
-                series1.setDataPointsRadius(7);
-                series1.setTitle(getString(R.string.road));
-                graphStatistics.addSeries((series1));
-                x_px_min = series1.getLowestValueX();
-                x_px_max = series1.getHighestValueX();
+                    // Series "Road"
+                    dataSeries = getData(2);
+                    if (dataSeries.length > tamHorizontalLabels)
+                        tamHorizontalLabels = dataSeries.length;
+                    LineGraphSeries<DataPoint> series1 = new LineGraphSeries<>(dataSeries);
+                    series1.setColor(Color.BLUE);
+                    series1.setDrawDataPoints(true);
+                    series1.setDataPointsRadius(7);
+                    series1.setTitle(getString(R.string.road));
+                    graphStatistics.addSeries((series1));
+                    x_px_min = series1.getLowestValueX();
+                    x_px_max = series1.getHighestValueX();
 
-                // Series "City"
-                dataSeries = getData(1);
-                if ( dataSeries.length > tamHorizontalLabels ) tamHorizontalLabels = dataSeries.length;
-                if ( tamHorizontalLabels > 10 ) tamHorizontalLabels = 10;
-                LineGraphSeries<DataPoint> series2 = new LineGraphSeries<>(dataSeries);
-                series2.setColor(Color.RED);
-                series2.setDrawDataPoints(true);
-                series2.setDataPointsRadius(7);
-                series2.setTitle(getString(R.string.city));
-                graphStatistics.addSeries((series2));
-                x_px_min = Math.min(series2.getLowestValueX(), x_px_min);
-                x_px_max = Math.max(series2.getHighestValueX(), x_px_max);
+                    // Series "City"
+                    dataSeries = getData(1);
+                    if (dataSeries.length > tamHorizontalLabels)
+                        tamHorizontalLabels = dataSeries.length;
+                    if (tamHorizontalLabels > 10) tamHorizontalLabels = 10;
+                    LineGraphSeries<DataPoint> series2 = new LineGraphSeries<>(dataSeries);
+                    series2.setColor(Color.RED);
+                    series2.setDrawDataPoints(true);
+                    series2.setDataPointsRadius(7);
+                    series2.setTitle(getString(R.string.city));
+                    graphStatistics.addSeries((series2));
+                    x_px_min = Math.min(series2.getLowestValueX(), x_px_min);
+                    x_px_max = Math.max(series2.getHighestValueX(), x_px_max);
 
-                // Prepare e Show Legend
-                //graphStatistics.getLegendRenderer().setVisible(true);
-                //graphStatistics.getLegendRenderer().setTextSize(20);
-                //graphStatistics.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+                    // Prepare e Show Legend
+                    //graphStatistics.getLegendRenderer().setVisible(true);
+                    //graphStatistics.getLegendRenderer().setTextSize(20);
+                    //graphStatistics.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
 
-                graphStatistics.getGridLabelRenderer().setVerticalLabelsColor(Color.BLUE);
-                graphStatistics.getGridLabelRenderer().setHorizontalLabelsColor(Color.BLUE);
-                graphStatistics.getGridLabelRenderer().setTextSize(20);
-                graphStatistics.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(graphStatistics.getContext(), new SimpleDateFormat("dd/MM")));
-                graphStatistics.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.BOTH);
-                graphStatistics.getGridLabelRenderer().setNumHorizontalLabels(tamHorizontalLabels);
-                graphStatistics.getGridLabelRenderer().setHumanRounding(false);
-                graphStatistics.getGridLabelRenderer().setVerticalAxisTitleTextSize(20);
-                graphStatistics.getGridLabelRenderer().setVerticalAxisTitleColor(R.color.design_default_color_secondary);
-                graphStatistics.getGridLabelRenderer().setVerticalAxisTitle(g.getMeasureConsumption());
+                    graphStatistics.getGridLabelRenderer().setVerticalLabelsColor(Color.BLUE);
+                    graphStatistics.getGridLabelRenderer().setHorizontalLabelsColor(Color.BLUE);
+                    graphStatistics.getGridLabelRenderer().setTextSize(20);
+                    graphStatistics.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(graphStatistics.getContext(), new SimpleDateFormat("dd/MM")));
+                    graphStatistics.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.BOTH);
+                    graphStatistics.getGridLabelRenderer().setNumHorizontalLabels(tamHorizontalLabels);
+                    graphStatistics.getGridLabelRenderer().setHumanRounding(false);
+                    graphStatistics.getGridLabelRenderer().setVerticalAxisTitleTextSize(20);
+                    graphStatistics.getGridLabelRenderer().setVerticalAxisTitleColor(R.color.design_default_color_secondary);
+                    graphStatistics.getGridLabelRenderer().setVerticalAxisTitle(g.getMeasureConsumption());
 
-                graphStatistics.onDataChanged(true,true);
-                graphStatistics.getViewport().setScalable(true);            //         activate horizontal zooming and scrolling
-                graphStatistics.getViewport().setScrollable(true);          //         activate horizontal scrolling
-                graphStatistics.getViewport().setScalableY(true);           //         activate horizontal and vertical zooming and scrolling
-                graphStatistics.getViewport().setScrollableY(true);         //         activate vertical scrolling
-                graphStatistics.getViewport().setXAxisBoundsManual(true);
-                graphStatistics.getViewport().setMinX(x_px_min);
-                graphStatistics.getViewport().setMaxX(x_px_max);
+                    graphStatistics.onDataChanged(true, true);
+                    graphStatistics.getViewport().setScalable(true);            //         activate horizontal zooming and scrolling
+                    graphStatistics.getViewport().setScrollable(true);          //         activate horizontal scrolling
+                    graphStatistics.getViewport().setScalableY(true);           //         activate horizontal and vertical zooming and scrolling
+                    graphStatistics.getViewport().setScrollableY(true);         //         activate vertical scrolling
+                    graphStatistics.getViewport().setXAxisBoundsManual(true);
+                    graphStatistics.getViewport().setMinX(x_px_min);
+                    graphStatistics.getViewport().setMaxX(x_px_max);
 
-                // In-Vehicle Services
+                    graphStatistics.refreshDrawableState();
+                } else {
+                    layerStatisticsVehicle.setVisibility(View.GONE);
+                }
+
+                // In-Vehicle Services - layerServiceVehicle
                 HomeVehicleMaintenanceListAdapter adapterMaintenance = new HomeVehicleMaintenanceListAdapter(Database.mMaintenanceDao.findReminderMaintenance( g.getIdVehicle() ), getContext());
-                maintenanceList.setAdapter(adapterMaintenance);
-                maintenanceList.setLayoutManager(new LinearLayoutManager(getContext()));
+                if (adapterMaintenance.getItemCount() > 0){
+                    layerServiceVehicle.setVisibility(View.VISIBLE);
+                    maintenanceList.setAdapter(adapterMaintenance);
+                    maintenanceList.setLayoutManager(new LinearLayoutManager(getContext()));
+                } else {
+                    layerServiceVehicle.setVisibility(View.GONE);
+                }
 
-                // Insurance
+                // Insurance - layerInsuranceVehicle
                 HomeVehicleInsuranceListAdapter adapterInsurance = new HomeVehicleInsuranceListAdapter(Database.mInsuranceDao.findReminderInsurance( g.getIdVehicle() ), getContext());
-                insuranceList.setAdapter(adapterInsurance);
-                insuranceList.setLayoutManager(new LinearLayoutManager(getContext()));
+                if ( adapterInsurance.getItemCount() > 0){
+                    layerInsuranceVehicle.setVisibility(View.VISIBLE);
+                    insuranceList.setAdapter(adapterInsurance);
+                    insuranceList.setLayoutManager(new LinearLayoutManager(getContext()));
+                }else {
+                    layerInsuranceVehicle.setVisibility(View.GONE);
+                }
 
                 adapterVehicle.notifyDataSetChanged();
                 adapterMaintenance.notifyDataSetChanged();
